e4e6e7082822407f48ef0c5d07c98b20066b479a
KseniyaU <140713902+KseniyaU@users.noreply.github.com>
2023-11-16 00:07:24 +0200

    task -02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector("#vimeo-player")
+const player = new Vimeo(iframe);
+let time;
+
+player.on('timeupdate', throttle(
+    (event) => {
+        localStorage.setItem("videoplayer-current-time", event)
+        // console.log(event);
+      }, 1000
+  ));
+time = localStorage.getItem("videoplayer-current-time");
+   console.log("time -" `${time}`);
