093e869802751b79e31b8462ad55d5ed7995c842
KseniyaU <140713902+KseniyaU@users.noreply.github.com>
2023-11-20 16:28:14 +0200

    м
    в

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,10 +7,33 @@ let time;
 
 player.on('timeupdate', throttle(
     (event) => {
-        time = localStorage.setItem( JSON(event.seconds))
-        console.log(time);
+    localStorage.setItem("videoplayer-current-time", `"${event.seconds}"`)
+    // time = localStorage.getItem("videoplayer-current-time")
+        // console.log(time);
         // console.log(event.seconds);// виводе час через 1 сек під час перегляду відео
       }, 1100
   ));
-// time = localStorage.getItem("videoplayer-current-time");
-   console.log("time -" `${time}`);
+
+//  console.log("time -" `${time}`);//час на якому закінчився перегляд
+time = JSON.parse(localStorage.getItem("videoplayer-current-time"))
+
+player.setCurrentTime(time).then(
+  function (seconds) {
+    seconds = time;
+  }
+).catch(
+  function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // seconds > time && seconds < time
+        break;
+      default:
+        break;
+    }
+  }
+);
+
+
+
+
+  
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
+
+const email = document.querySelector("input");
+const textarea = document.querySelector("textarea");
+const form = document.querySelector(".feedback-form")
+
+let inform = {
+    email: "",
+    message: "",
+};
+const KEY = "feedback-form-state";
+
+
+
+form.addEventListener('input', throttle (
+    (event) => {
+        inform[event.target.name] = event.target.value;
+        localStorage.setItem(KEY, JSON.stringify({... inform, email: email.value, message: textarea.value}))
+    },500
+)
+)
+// textarea.addEventListener('input',
+//     (event) => {
+//         inform.message = event.currentTarget.value
+//     }
+// )
+
+console.log(inform);
+// email.addEventListener('input', throttle(
+//     (event) => {
+//         console.log(event.currentTarget.value);
+//         if (event.currentTarget.value !== " ") {
+
+//             localStorage.setItem("feedback-form-state", event.currentTarget.value)
+//         }
+//     }, 500
+// ))
+// let textEmail;
 
